8f77aa5d86961e3e7ab3ca2104b192f67a99f171
Aleksander Thornes Vestlund <aleksander.vestlund@hotmail.com>
2024-07-08 13:37:30 +0200

    Remove exercises to please Big Poppa

--- exercises/exercise1/take_pieces.py
+++
@@ -1,2 +0,0 @@
-def take_pieces(n_pieces: int) -> int:
-    return max(1, (n_pieces - 1) % 8)
--- exercises/exercise6/f.py
+++
@@ -1,5 +0,0 @@
-from math import comb
-
-
-def f(x: int, y: int) -> int:
-    return comb(x + y - 2, x - 1)
