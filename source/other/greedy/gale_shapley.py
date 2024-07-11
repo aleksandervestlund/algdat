dece25f5addd117b893c9ae9cfdc5b9b95946f6e
Aleksander Thornes Vestlund <aleksander.vestlund@hotmail.com>
2024-07-11 14:06:23 +0200

    Improve Galeren

--- source/other/greedy/gale_shapley.py
+++ source/other/greedy/gale_shapley.py
@@ -1,25 +1,26 @@
 def gale_shapley(
     men: list[list[int]], women: list[list[int]]
 ) -> dict[int, int]:
+    """Female-oriented Gale-Shapley algorithm.
+
+    Runtime: O(n^2).
+    """
     n = len(women)
     free_ws = set(range(n))
-    engagements: dict[int, int] = {}  # woman: man
+    engagements: dict[int, int] = {}  # man: woman
 
     while free_ws:
         w = free_ws.pop()
 
         for m in women[w]:
             priority = men[m]
-            wife = next(
-                (woman for woman, man in engagements.items() if man == m),
-                None,
-            )
 
-            if wife is None:
-                engagements[w] = m
-            elif priority.index(w) < priority.index(wife):
+            if (wife := engagements.get(m)) is None:
+                engagements[m] = w
+                break
+            if priority.index(w) < priority.index(wife):
                 free_ws.add(wife)
-                engagements.pop(wife)
-                engagements[w] = m
+                engagements[m] = w
+                break
 
-    return engagements
+    return {w: m for m, w in engagements.items()}  # woman: man
